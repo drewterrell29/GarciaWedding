02bb88f37e27118ac275ba09e0a360b1da833bd0
Drew Terrell <drewterrell29@gmail.com>
2015-03-03 07:12:30 -0600

    Added a google map, photos, and part of the registry.

---
+++ js/googlemaps.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var myLatLang = new google.maps.LatLng(32.92298778, -96.7780602)
+    var mapOptions = {
+      center: myLatLang,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+    
+    var marker = new google.maps.Marker({
+        position: myLatLang,
+        map: map,
+        title: 'Wedding!'
+    });
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
