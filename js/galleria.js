28a008f8d94a3f7d038de3b7c170f303e7bc0427
Drew Terrell <drewterrell29@gmail.com>
2015-04-06 17:36:21 -0500

    Fixed the missing galleria.js and modified the margins a bit with the photo viewer.

---
+++ js/galleria.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+function galleria() {
+	Galleria.loadTheme('galleria/themes/classic/galleria.classic.min.js');
+	Galleria.run('#galleria', {
+		flickr: 'set:72157651388168108',
+		flickrOptions: {
+		sort: 'date-posted-asc'
+		}
+	});
+}
+
+window.onload = galleria;
